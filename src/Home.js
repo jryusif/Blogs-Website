186a35e259435db0dd3c36f513a9d76439675903
Mohamed Yousef <63268675+jryusif@users.noreply.github.com>
2022-02-20 08:38:52 +0200

    home and new blog pages

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import BlogList from "./Bloglist";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
+   
+
+        
+
+    return ( 
+        <div className="home">
+            {error && <div> {error}</div>}
+            { isPending && <div> Loading...</div>}
+            { blogs&& <BlogList blogs = {blogs} title = "All Blogs!!" /> }  
+            { blogs&& <BlogList blogs = {blogs.filter( (blog) => blog.author === "mario")} title = "Mario's blogs"/>} 
+
+           </div>
+      
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+
+
+
+const  Navbar = () => {
+    return ( 
+<nav className="navbar">
+    <h1>  Company </h1>
+    <div className="links">
+    <a href="/"> Home </a>
+    <a href="/create"> New Blog </a>
+
+    </div>
+</nav>     );
+}
+ 
+export default Navbar;
